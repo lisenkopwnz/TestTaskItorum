863cb3318cee86dd062d04dac60f4b51c6f27dce
unknown <pal3noa@ya.ru>
2025-02-19 19:27:37 +0300

    Create serializer

--- email_campaigns/messaging/models.py
+++ email_campaigns/messaging/models.py
@@ -11,12 +11,12 @@ class Client(models.Model):
     def __str__(self):
         return f'Client id-{self.pk}, phone number-{self.phone_number}'
 
-class Camping(models.Model):
+class Campaign(models.Model):
     start_time = models.DateTimeField(verbose_name="Start Time")
     end_time = models.DateTimeField(verbose_name="End Time")
     message_text = models.TextField(verbose_name="Message Text")
-    operator_code_filter = models.CharField(max_length=3, blank=True, null=True, verbose_name="Operator Code")
-    tag_filter = models.CharField(max_length=255, blank=True, null=True, verbose_name="Tag")
+    operator_code_filter = models.CharField(max_length=3, verbose_name="Operator Code")
+    tag_filter = models.CharField(max_length=255, verbose_name="Tag")
 
     def __str__(self):
         return f"Campaign id-{self.pk} - {self.start_time} to {self.end_time}"
---
+++ email_campaigns/messaging/serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from messaging.models import Client, Campaign, Message
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['id', 'phone_number', 'operator_code', 'tag']
+
+class CampaignSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Campaign
+        fields = ['id', 'start_time', 'end_time', 'message_text', 'operator_code_filter', 'tag_filter']
+
+class MessageSerializer(serializers.ModelSerializer):
+    campaign = CampaignSerializer()
+    client = ClientSerializer()
+    
+    class Meta:
+        model = Message
+        fields = ['id', 'send_time', 'campaign', 'client']
