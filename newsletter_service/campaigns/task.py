74f9c39315f919000ef445a489e900ba2df36ef6
unknown <pal3noa@ya.ru>
2025-02-20 14:04:50 +0300

    create celery task

--- newsletter_service/campaigns/task.py
+++ newsletter_service/campaigns/task.py
@@ -11,13 +11,22 @@ def send_mailing(mailing_id):
     """Задача отправки рассылки"""
     try:
         mailing = Campaign.objects.get(id=mailing_id)
-        if mailing.start_time <= now() <= mailing.end_time:
+        current_time = now()
+
+        # Проверяем, что текущее время находится в пределах допустимого интервала рассылки
+        if mailing.start_time <= current_time <= mailing.end_time:
             clients = Client.objects.filter(
                 operator_code=mailing.operator_code,
                 tag=mailing.tag
             )
+
             for client in clients:
-                # Имитация отправки сообщения (можно заменить реальной логикой)
+                # Прерываем рассылку, если время завершения прошло
+                if now() > mailing.end_time:
+                    print(f"Время рассылки истекло. Прекращаем отправку.")
+                    break  # Прерываем цикл отправки сообщений
+
+                # Имитация отправки сообщения
                 print(f"Отправка: {mailing.text} -> {client.phone_number}")
                 Message.objects.create(
                     mailing=mailing,
@@ -25,6 +34,6 @@ def send_mailing(mailing_id):
                     sent_at=now()
                 )
         else:
-            print("Время рассылки истекло или ещё не наступило.")
+            print("Время рассылки не в пределах допустимого интервала.")
     except Campaign.DoesNotExist:
         print("Рассылка не найдена.")
